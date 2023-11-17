923a5409f864fa501fc208c99088bc991ea6b6ec
Mart510 <martynandrewbillings@gmail.com>
2023-11-17 12:13:29 +0000

    adding comp with default props for testing

--- currency-convertor/app/components/Output.js
+++ currency-convertor/app/components/Output.js
@@ -1,7 +1,16 @@
-
-
-export default function Output(){
+export default function Output({props}){
+    // we need to check that this is the correct order that the information is coming to us in.
+    const {homeCurrency, holidayCurrency, conversionRate, expectedTotal} = props
     return (
+        <>
         <div>This is the Output</div>
+        <p> The conversion rate between {homeCurrency} and {holidayCurrency} is {conversionRate}.</p>
+        <p> Your expected total is {expectedTotal}.</p>
+        </>
+
     )
 }
+
+// default props for testing
+
+Output.defaultprops = {homeCurrency: 1, holidayCurrency: 2, conversionRate: (homeCurrency/holidayCurrency), expectedTotal: (3000)}
