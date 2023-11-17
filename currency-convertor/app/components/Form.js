c50719160edfda5976a3af8a3e87586ec7ee61cd
Ana Raducanu <danuta.anna@yahoo.com>
2023-11-17 12:07:11 +0000

    added the state for input and currencySelection

--- currency-convertor/app/components/Form.js
+++ currency-convertor/app/components/Form.js
@@ -1,4 +1,13 @@
+// importing useState, useEffect for fetching data
+import { useState, useEffect } from "react";
 export default function Form() {
+  // 1. When the user inputs a number and then selects a currency, the amount should be displayed and vice-versa
+  // initial state of input is 0
+  const [input, setInput] = useState(0);
+  // initial state of currency is EUR
+  const [currencySelection, setCurrencySelection] = useState("EUR -Euro");
+  // a function that will handle the input change and the currency selection
+  // a function that will handle the currency convertion
   return (
     <form>
       <label>Add amount £ </label>
